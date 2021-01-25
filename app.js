db1c5991386d1c6fb61ddbac84f04303c8cd2043
hadiyouness10 <hadi.youness@lau.edu>
2021-01-25 18:06:23 +0200

    s

--- app.js
+++ app.js
@@ -4,7 +4,11 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const encrypt = require("mongoose-encryption")
+const md5 = require("md5");
+const session = require("express-session");
+const passport = require("passport");
+const passportLocalMongoose = require("passport-local-mongoose");
+
 
 const app = express();
 
@@ -12,6 +16,13 @@ app.use(express.static("public"));
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: true }));
 
+app.use(session({
+    secret: "Our little secret";
+    resave: false,
+    saveUninitialized: false
+}));
+
+app.use(passport.initialize());
 
 mongoose.connect("mongodb://localhost:27017/userDB", { useNewUrlParser: true });
 
@@ -23,7 +34,7 @@ const userSchema = new mongoose.Schema({
 });
 
 const secret = process.env.SECRET;
-userSchema.plugin(encrypt, { secret: secret, encryptedFields: ['password'] });
+// userSchema.plugin(encrypt, { secret: secret, encryptedFields: ['password'] });
 
 
 const User = new mongoose.model("User", userSchema);
@@ -43,36 +54,39 @@ app.get("/register", function(req, res) {
 });
 
 
+// app.post("/register", function(req, res) {
+//     const newUser = new User({
+//         email: req.body.username,
+//         password: md5(req.body.password)
+//     });
+//     newUser.save(function(err) {
+//         if (err) console.log(err);
+//         else res.render("secrets");
+//     });
+
+// })
+
+// app.post("/login", function(req, res) {
+//     const username = req.body.username;
+//     const password = md5(req.body.password);
+
+//     User.findOne({ email: username }, function(err, foundUser) {
+//         if (err) console.log(err);
+//         else {
+//             if (foundUser) {
+//                 if (foundUser.password == password) {
+//                     res.render("secrets");
+//                 }
+//             }
+//         }
+//     })
+// })
+
+
 app.post("/register", function(req, res) {
-    const newUser = new User({
-        email: req.body.username,
-        password: req.body.password
-    });
-    newUser.save(function(err) {
-        if (err) console.log(err);
-        else res.render("secrets");
-    });
-
-})
-
-app.post("/login", function(req, res) {
-    const username = req.body.username;
-    const password = req.body.password;
-
-    User.findOne({ email: username }, function(err, foundUser) {
-        if (err) console.log(err);
-        else {
-            if (foundUser) {
-                if (foundUser.password == password) {
-                    res.render("secrets");
-                }
-            }
-        }
-    })
-})
 
 
-
+});
 
 
 
